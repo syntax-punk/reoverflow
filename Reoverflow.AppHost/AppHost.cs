5900b16517822ae355f940b943bbcc40fee667c6
syntax-punk <david.jaeren@gmail.com>
2025-12-30 22:56:46 +0100

    Adding Question service

--- Reoverflow.AppHost/AppHost.cs
+++ Reoverflow.AppHost/AppHost.cs
@@ -3,4 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak-data");
 
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WaitFor(keycloak);
+
 builder.Build().Run();
