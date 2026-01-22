67ec7329112b59afc57dfd3f19794970a97d521d
syntax-punk <david.jaeren@gmail.com>
2026-01-22 23:19:34 +0100

    Typesense collection wip

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,3 +1,5 @@
+using SearchService.Data;
+using Typesense;
 using Typesense.Setup;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,4 +36,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultEndpoints();
+
+using var scope = app.Services.CreateScope();
+var client = scope.ServiceProvider.GetRequiredService<ITypesenseClient>();
+await SearchInitializer.EnsureIndexExists(client);
+
 app.Run();
