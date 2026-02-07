218da98ab6e1c1049b2042622822434c7cb992f8
syntax-punk <david.jaeren@gmail.com>
2026-02-07 21:29:58 +0100

    Added a question hadnler in the search service

---
+++ SearchService/MessageHandlers/QuestionCreatedHandler.cs
@@ -0,0 +1,30 @@
+using System.Text.RegularExpressions;
+using Contracts;
+using SearchService.Models;
+using Typesense;
+
+namespace SearchService.MessageHandlers;
+
+public class QuestionCreatedHandler(ITypesenseClient client)
+{
+    public async Task HandleAsync(QuestionCreated message)
+    {
+        var created = new DateTimeOffset(message.Created).ToUnixTimeSeconds();
+        var doc = new SearchQuestion
+        {
+            Id = message.QuestionId,
+            Title = message.Title,
+            Content = StripHtml(message.Content),
+            CreatedAt = created,
+            Tags = message.Tags.ToArray()
+        };
+        await client.CreateDocument("questions", doc);
+        
+        Console.WriteLine($"Created question: {message.QuestionId}");
+    }
+
+    private static string StripHtml(string content)
+    {
+        return Regex.Replace(content, "<.*?>", string.Empty);
+    }
+}
