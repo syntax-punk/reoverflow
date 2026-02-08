099f9c9716fffeedaa48ca45a71bcc3a679888d8
syntax-punk <david.jaeren@gmail.com>
2026-02-08 23:22:16 +0100

    For some reason my messagess are not published

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -1,17 +1,18 @@
 using System.Security.Claims;
+using Contracts;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuestionService.Data;
 using QuestionService.DTOs;
 using QuestionService.Models;
+using Wolverine;
 
 namespace QuestionService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class QuestionsController(QuestionDbContext db) : ControllerBase
+public class QuestionsController(QuestionDbContext db, IMessageBus bus) : ControllerBase
 {
     [Authorize]
     [HttpPost]
@@ -38,6 +39,9 @@ public class QuestionsController(QuestionDbContext db) : ControllerBase
         
         db.Questions.Add(question);
         await db.SaveChangesAsync();
+        
+        await bus.PublishAsync(new QuestionCreated(question.Id, question.Title, question.Content, 
+            question.CreatedAt, question.TagSlugs));
         
         return Created($"questions/{question.Id}", question);
     }
